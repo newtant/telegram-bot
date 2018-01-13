e642f6888bdf464ae6ae0d59d85649ae7bc9cbe4
newtant <the.newtant@gmail.com>
2018-01-13 12:30:38 +0100

    works, cleaner, success

--- hermes.py
+++ hermes.py
@@ -6,6 +6,13 @@ URL = 'https://api.telegram.org/bot{}/'.format(config.token)
 self_chat = config.self_chat
 
 #####################
+####### TO-DO #######
+#####################
+
+# apparently requests has json decoding builtin via .json()
+# still can't handle stickers/unicode whatsoever
+
+#####################
 ### BOT FUNCTIONS ###
 #####################
 
@@ -19,36 +26,30 @@ def getPublicIP():
 ######################
 
 def getUpdates(offset=None):
-    #apparently requests has json decoding builtin via .json()
-    #TODO
     request = URL + 'getUpdates'
     if offset:
         request += '?offset={}'.format(int(offset)+1)
     updates = getUrl(request)
     updates = json.loads(updates.text)
-    if updates['result']:
-        return updates['result']
+    return updates
 
 
 def parseMessages(updates):
     messages = []
-    new_update = updates[-1]['update_id']
+    new_update = updates['result'][-1]['update_id']
     # print("Last Update ID: {}".format(new_update))
-    for result in updates:
+    saveNewUpdate(new_update)
+    for result in updates['result']:
         try:
             messages.append(result['message']['text'])
         except:
             print("Unicode, can't parse.")
-    saveNewUpdate(new_update)
-    return messages
 
     for message in messages:
-        print("Received message: \"{}\"".format(message))
-
+        print("Received message: \"{}\"".format(message.lower()))
         if message.lower() == 'ip':
             sendMessage(getPublicIP(), self_chat)
-
-        if message.lower() == 'test':
+        elif message.lower() == 'test':
             sendMessage("I'm awake.", self_chat)
 
 
@@ -64,13 +65,14 @@ def saveNewUpdate(id):
 
 
 def sendMessage(message, chat):
+    # print("Sending...")
     request = URL + 'sendMessage?chat_id={}&text={}'.format(chat, message)
-    # print(getUrl(request).status_code)
+    print(getUrl(request).status_code)
 
 
 if __name__ == '__main__':
     with open('update_id.txt', 'r') as logFile:
         last_update = logFile.readline()
-    print('Fetching new messages, last Update ID was {}.'.format(last_update))
+    # print('Fetching new messages, last Update ID was {}.'.format(last_update))
     updates = getUpdates(last_update)
     parseMessages(updates)
